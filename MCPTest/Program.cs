7929e413730ef3eaeea94f6814a1714c4b6d02ba
Aarthi Murugan <cr.aarthi@gmail.com>
2025-05-30 19:28:21 -0400

    first commit

---
+++ MCPTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"hello {message}";
+}
+
+[McpServerToolType]
+public static class ReverseTool
+{
+    [McpServerTool, Description("Reverse the message back to the client.")]
+    public static string Reverse(string message) => new string(message.Reverse().ToArray());
+}
+
+[McpServerToolType]
+public static class LengthTool
+{
+    [McpServerTool, Description("Returns the length of the message.")]
+    public static int GetLength(string message) => message.Length;
+}
